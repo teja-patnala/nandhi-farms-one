e0f0b214225ecf302d21dfda342dbff958d7637b
teja_patnala <patnalatejaa@gmail.com>
2024-04-02 11:32:59 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import {initializeApp} from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
- 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCInNIs8Eyza0WwU4CeHR45Y01Q0LDU5UE",
-  authDomain: "teja-86afe.firebaseapp.com",
-  projectId: "teja-86afe",
-  storageBucket: "teja-86afe.appspot.com",
-  messagingSenderId: "396230478134",
-  appId: "1:396230478134:web:769d71ac90fc7df2944b97"
+  apiKey: "AIzaSyCU16E0jVMZCUM-WYJuR_hb_lLKEdb-yEY",
+  authDomain: "peoject-nandhi-farms.firebaseapp.com",
+  projectId: "peoject-nandhi-farms",
+  storageBucket: "peoject-nandhi-farms.appspot.com",
+  messagingSenderId: "321513259436",
+  appId: "1:321513259436:web:7217b1a6b092a8af7fba09"
 };
 
 
